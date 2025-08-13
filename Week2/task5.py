121e9c6a99a91d99178be2a0c1927bb8a2b7dacf
ridox ayomide <ayomideridox@gmail.com>
2025-08-13 12:23:13 +0100

    prog

--- Week2/task5.py
+++ Week2/task5.py
@@ -1,9 +1,38 @@
-favorite_food1 = input("enter favorite dish: ")
-favorite_food2 = input("enter favorite dish: ")
-favorite_food3 = input("enter favorite dish: ")
-dishes = []
-dishes.append(favorite_food1)
-dishes.append(favorite_food2)
-dishes.append(favorite_food3)
-print(tuple(dishes))
-print("\n".join(dishes))
+# #
+# favorite_food1 = input("enter favorite dish: ")
+# favorite_food2 = input("enter favorite dish: ")
+# favorite_food3 = input("enter favorite dish: ")
+# dishes = []
+# dishes.append(favorite_food1)
+# dishes.append(favorite_food2)
+# dishes.append(favorite_food3)
+# print(tuple(dishes))
+# print("\n".join(dishes))
+
+# # Task 2
+# best_friend_name1 = input("Enter the name of your bestfriend: ")
+# best_friend_name2 = input("Enter the name of your bestfriend: ")
+# best_friend_name3 = input("Enter the name of your bestfriend: ")
+# best_friend_name4 = input("Enter the name of your bestfriend: ")
+# best_friend_name5 = input("Enter the name of your bestfriend: ")
+# friends = (best_friend_name1, best_friend_name2, best_friend_name3, best_friend_name4, best_friend_name5)
+# print(friends)
+# reversed_friend_name = friends[::-1]
+# print(reversed_friend_name)
+
+#Task 3
+states =[]
+State1 = input("Enter your state: ")
+State2 = input("Enter your state: ")
+State3 = input("Enter your state: ")
+State4 = input("Enter your state: ")
+State5 = input("Enter your state: ")
+print(State1,State5)
+print("lagos" in State1,State2,State3,State4,State5) 
+print("lagos" not in State1,State2,State3,State4,State5)
+states.append(State1)
+states.append(State2)
+states.append(State3)
+states.append(State4)
+states.append(states)
+print()
