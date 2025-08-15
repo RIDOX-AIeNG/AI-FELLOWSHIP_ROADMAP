5aaa596e14bb7aef089eae4144ef4909da5ee8c1
ridox ayomide <ayomideridox@gmail.com>
2025-08-15 14:37:46 +0100

    working with dictionaries

--- TASK_FOLDER/Task6/UniqueNameCollecter.py
+++ TASK_FOLDER/Task6/UniqueNameCollecter.py
@@ -1,10 +1,19 @@
 #Task2
 #Unique name collector- Attendees of a seminar
 #Create an empty attendees set
+# attendees = set()
+# #add names to the set
+# attendees.add("Rukhayah")
+# attendees.add("A'ishah")
+# attendees.add("Badrudeen")
+# #print the attendees 
+# print("Attendees:", attendees)
+
 attendees = set()
 #add names to the set
-attendees.add("Rukhayah")
-attendees.add("A'ishah")
-attendees.add("Badrudeen")
+att1 = attendees.add(input("Enter your name: "))
+atte = attendees.add(input("Enter your name: "))
+att = attendees.add(input("Enter your name: "))
 #print the attendees 
-print("Attendees:", attendees)
+seminar_att = attendees
+print("seminar attendees:", sorted(seminar_att, key= lambda x:x.lower()))
