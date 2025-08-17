922ce86dacbddbd7835841769334a8d39c147ebc
Ridwan <ayomideridox@gmail.com>
2025-08-17 07:01:16 +0100

    updating question 2 of task 7 week 2

--- TASK_FOLDER/Task7/SuperMarketPriceList.py
+++ TASK_FOLDER/Task7/SuperMarketPriceList.py
@@ -1,22 +1,27 @@
-supermarket_pricelist = {} 
-items = ["Maggi","Sugar", "salt", "Spoon", "milk"]
-Maggi_price = float(input("Enter the price: "))
-Sugar_price = float(input("Enter the price: "))
-salt_price = float(input("Enter the price: "))
-spoon_price = float(input("Enter the price: "))
-Milk_price = float(input("Enter the price: "))
-print(f"Bread: {Maggi_price}")
-print(f"Burger: {Sugar_price}")
-print(f"rice: {salt_price}")
-print(f"garri: {spoon_price}")
-print(f"Milk: {Milk_price}")
-print("\nAvailable Items: ", list(supermarket_pricelist.keys()))
-items_to_update = input("Enter item to be updated :")
-while items_to_update not in supermarket_pricelist:
-    print("item not available,select from:", list(supermarket_pricelist.keys()))
-    items_to_update = input("Enter item to be updated :")
-new_price = input(f" Enter new price {items_to_update}:")
-supermarket_pricelist.update({items_to_update:new_price})
-print("\nUpdated pricelist:")
-for item,price in supermarket_pricelist.items():
-    print(f"{item}:{price}")
+#Supermarket Price list
+#Create a dictionary that stores the items and their prices.
+#Create a function that allow users to enter and update the prices.
+item = {} 
+item1 = ("Maggi")
+item2 = ("Sugar")
+item3 = ("salt")
+item4 = ("spoon")
+item5 = ("Milk")
+sup = (item1,item2,item3,item4,item5)
+for key in item:
+    print(key)
+price={}
+Maggi_price = input("Enter the price: ")
+Sugar_price = input("Enter the price: ")
+salt_price = input("Enter the price: ")
+spoon_price = input("Enter the price: ")
+Milk_price = input("Enter the price: ")
+price = {Maggi_price,Sugar_price,salt_price,spoon_price,Milk_price}
+print(f"\nAvailable Items and their prices:\n{item1}: #{Maggi_price}\n{item2}: #{Sugar_price}\n{item3}: #{salt_price}\n{item4}: #{spoon_price}\n{item5}: #{Milk_price}\n")
+updatedprice = input("Enter the new price of Maggi: #")
+price.update(updatedprice)
+Maggi_price = updatedprice
+print(f"The new price of Maggi is: #{updatedprice}")
+print("\nThe new price list for each items are:")
+print(f"{item1}: #{updatedprice}\n{item2}: #{Sugar_price}\n{item3}: #{salt_price}\n{item4}: #{spoon_price}\n{item5}: #{Milk_price}\n")
+
