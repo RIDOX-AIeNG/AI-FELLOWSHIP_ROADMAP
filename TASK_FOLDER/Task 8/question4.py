93eccce4e68b5749a282c7b490f8522bda34bde5
ridox ayomide <ayomideridox@gmail.com>
2025-08-18 15:14:57 +0100

    age

--- TASK_FOLDER/Task 8/question4.py
+++ TASK_FOLDER/Task 8/question4.py
@@ -1,9 +1,12 @@
 student = {}
 name = input("enter your name: ")
-Age = input("Enter your age: ")
+Age = int(input("Enter your age: "))
 student["name"] = name
 student["age"] = Age
 scores = [34,56,79]
 student["score"] = scores
 average = sum(scores)/len(scores)
-student["passed"] = average
+student["passed"] = average >= 50
+student["Teenage"]= (Age > 13) and (Age < 19)
+print(f"student Record\nname: {student['name']}\nAge: {student["age"]}\nScore: {student["score"]}\nPassed:{student["passed"]}\nTeenage: {student["Teenage"]}")
+
