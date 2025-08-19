b3a9fc47c0c18605e1a874fdd1ce86348998f847
ridox ayomide <ayomideridox@gmail.com>
2025-08-19 10:03:23 +0100

    task8

---
+++ TASK_FOLDER/Task 8/question5.py
@@ -0,0 +1,6 @@
+store = {"Book": 10, "Pen": 20, "Bag": 5}
+item_to_buy = input("Enter item: ")
+Quantity = input("Enter quantity: ")
+print("Before Purchase: ", store)
+store[item_to_buy] -= Quantity
+print("After purchase: ", store)
